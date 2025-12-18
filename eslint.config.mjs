41d2a7f9f507c716157a6c3ca6f6ed1bc0b2167b
Simone Gauli <simonegauli@gmail.com>
2025-12-18 14:12:15 +0100

    perfect

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
 import { reactConfigs } from '@goatjs/react-library-eslint';
 import { defineConfig, globalIgnores } from '@eslint/config-helpers';
 
-export default defineConfig([globalIgnores(['dist']), ...reactConfigs({ tsconfigRootDir: import.meta.dirname })]);
+export default defineConfig([globalIgnores(['dist', '.yarn', 'coverage']), ...reactConfigs({ tsconfigRootDir: import.meta.dirname })]);
